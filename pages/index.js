369058f184e2eeb80a235476af6edd61a67d1d21
Welbert Filho <welbertfilho9@gmail.com>
2021-01-27 13:31:32 -0300

    aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+           <Widget.Header>
+              <h1>
+                SPFC
+              </h1>
+            </Widget.Header>
+          <Widget.Content>
+            <p>
+              Tri campeão mundial
+           </p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>
+              Quiz dos gambás
+           </h1>
+            <p>
+              Rebaixado
+           </p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://www.instagram.com/saopaulofc/?hl=pt-br" />
+    </QuizBackground>
+  );
 }
